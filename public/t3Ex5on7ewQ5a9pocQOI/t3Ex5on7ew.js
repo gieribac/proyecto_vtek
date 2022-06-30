b68bf00046b32eb0762082668afdc01b0583575b
Jalkejuan <juancucunuba5@gmail.com>
2022-06-30 10:40:23 -0500

    1

---
+++ public/t3Ex5on7ewQ5a9pocQOI/t3Ex5on7ew.js
@@ -0,0 +1,14 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAcRK2u7M_KhqbOTNRmEJ86o6k6jfFwlrQ",
+    authDomain: "vtek-4acd8.firebaseapp.com",
+    projectId: "vtek-4acd8",
+    storageBucket: "vtek-4acd8.appspot.com",
+    messagingSenderId: "479841844049",
+    appId: "1:479841844049:web:e0bebca54add45565d3170"
+};
+const firebaseApp = firebase.initializeApp({ firebaseConfig });
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
