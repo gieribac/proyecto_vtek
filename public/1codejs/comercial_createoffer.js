cf8698d260cb90abc0259841414f0d2849f4e949
gieribac <geibaneza@unal.edu.co>
2022-08-09 20:41:27 -0500

    se avanza back crear o editar oferta

--- public/1codejs/comercial_createoffer.js
+++ public/1codejs/comercial_createoffer.js
@@ -1,14 +1,59 @@
-import { setOferta } from "./models/post";
+import { setOferta, queryOferta } from "./models/post";
 
 const observerdatos = new MutationObserver(()=>{ 
     
-    const charge = () => {
+    const charge = () => {     
+        
+        
+        ///escoger oferta///
+        const ds = localStorage.getItem("of",mapOf)
+        ds.forEach(d => {  
+            viewO.innerHTML = `
+            <h6 >${d.data().Oferta}</h6>`
+        })
+        //ver "navegacion por tabs" https://youtu.be/jOj6Ouvlq6o?list=PLIe7HjzbbZwOLhbL08lqTI-sdcVKuwga4
+        const viewO = document.getElementById('viewo');//contenedor de lista de ofertas
+        let ofertas = Array.prototype.slice.apply(document.querySelectorAll("#viewo > h6"));
+        viewO.addEventListener('click', e => {
+            if (e.target.classList.contains("h6")){
+                let i = ofertas.indexOf(e.target);
+                ofertas.map( t => t.classList.remove("active_o"));
+                ofertas[i].classList.add("active_o");
+
+
+            }
+        })
+        let ofertaactiva = document.getElementsByClassName("active_o");
+        queryOfertaActiva(ofertaactiva.value).then( ds => {
+            const cliente = document.getElementsByName("cliente");
+            const producto = document.getElementsByName("producto");
+            const fabrica = document.getElementsByName("fabrica");
+            const vigencia = document.getElementsByName("vigencia");
+            const esquema = document.getElementsByName("esquema");
+            const vs = document.getElementsByName("vs");
+            const centrocosto = document.getElementsByName("centrocosto");
+            const comercial = document.getElementsByName("comercial");
+
+            ds.forEach(d => {
+                cliente.placeholder = d.data().Cliente;
+                producto.placeholder = d.data().Producto;
+                fabrica.placeholder = d.data().Fabrica;
+                vigencia.placeholder = d.data().Vigencia;
+                esquema.placeholder = d.data().Esquema;
+                vs.placeholder = d.data().Vigilancia_Seguimiento;
+                centrocosto.placeholder = d.data().Centro_Costo;
+                comercial.placeholder = d.data().Comercial;
+             })  
+
+
+        })
+
+        //validacion de formulario//
         const formulario = document.getElementById('formCO');
         const save = document.getElementById('guardarO');
         save.disabled = true;
         const entradas = document.querySelectorAll('input');
-        
-        
+
         entradas.forEach(ev=>{  
             let estadoentradas = true;
                 
