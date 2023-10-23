375c95fa4fcffff40d0b1495db7a0411ec8ff687
gieribac <geibaneza@unal.edu.co>
2023-10-23 11:01:23 -0500

    seguro

--- public/1codejs/firebase.js
+++ public/1codejs/firebase.js
@@ -1,17 +1,17 @@
 
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
 import {getStorage} from "https://www.gstatic.com/firebasejs/9.9.0/firebase-storage.js"; //storage
-import {getAuth, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/9.9.0/firebase-auth.js"; //Authentication
+import {getAuth} from "https://www.gstatic.com/firebasejs/9.9.0/firebase-auth.js"; //Authentication
 import {getFirestore} from "https://www.gstatic.com/firebasejs/9.9.0/firebase-firestore.js"; //Firestore Database
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAcRK2u7M_KhqbOTNRmEJ86o6k6jfFwlrQ",
-  authDomain: "vtek-4acd8.firebaseapp.com",
-  databaseURL: "https://vtek-4acd8-default-rtdb.firebaseio.com",
-  projectId: "vtek-4acd8",
-  storageBucket: "vtek-4acd8.appspot.com",
-  messagingSenderId: "479841844049",
-  appId: "1:479841844049:web:e0bebca54add45565d3170"
+  apiKey: "adsdasdasdasdasdsa_fsdfdsafdssqwqwweeqqq",
+  authDomain: "qweqweqweccaopopupdfykufe",
+  databaseURL: "https://rwrwejkdfñji3tcr3jvtg348.com",
+  projectId: "wreewrwe",
+  storageBucket: "sdderccccccccccccc.com",
+  messagingSenderId: "1111111111114981787",
+  appId: "7:2342342342342343243:wib:a1ggdfgdfgdftru54654brt554gg54540"
 };
 
 export const app = initializeApp(firebaseConfig);
